0791f428bae0e50ac0c933ade6fd4354ecbd1cdf
diondharmawan <diondharmawan77@gmail.com>
2025-01-29 22:19:12 +0700

    tambah gambars

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ conn = st.connection("gsheets", type=GSheetsConnection)
 st.header("Realisasi Anggaran Pembangunan Gereja")
 st.subheader("Gereja Katolik Roh Kudus Kebonarum")
 st.write("Dion Dharmawan")
+st.image(gereja.jpg)
 
 sql = '''
 SELECT
